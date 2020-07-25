6be22e37240fd01526518f138ef322df0002d387
Vyacheslav <tarakanov.ekb@gmail.com>
2020-07-25 22:03:38 +0300

    Home task II

---
+++ src/Home task II.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Home.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main()	{
+	printf ("Hello world!\n");
+	return 0;
+}
