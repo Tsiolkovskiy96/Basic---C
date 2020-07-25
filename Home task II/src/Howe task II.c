ae4d4dd8a3517c232adc9851d0d4c3720fec3061
Vyacheslav <tarakanov.ekb@gmail.com>
2020-07-25 22:43:22 +0300

    Home task II (Corrected)

--- Home task II/src/Howe task II.c
+++ Home task II/src/Howe task II.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : Home.c
+ Name        : Howe.c
  Author      : 
  Version     :
  Copyright   : Your copyright notice
@@ -11,6 +11,17 @@
 #include <stdio.h>
 
 int main()	{
-	printf ("Hello world!\n");
+
+	// First task
+	printf ("Task I\n");
+	int value;
+	printf ("Please select whole value from 0 to 100 : ");
+	scanf ("%d", &value);
+	printf ("%s", (value >= 0) && (value <= 100) ? "Right choice\n" : "Negative choice\n");
+
+	//Second task
+	printf (" \n");
+	printf ("Task II \n");
 	return 0;
 }
+
