c69a605cd6fdcd0a733964070ba7542a96d67647
Vyacheslav <tarakanov.ekb@gmail.com>
2020-07-30 22:05:47 +0300

    Home task III (Start)

---
+++ Home task III/src/Home task III.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Home task III.c
+ Author      : Vyacheslav Tarakanov
+ Version     : v1.0
+ Copyright   : Only to me
+ Description : Home task III
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
