81fbb572d58c6d931eeac97696d67c691d07aa17
Vyacheslav <tarakanov.ekb@gmail.com>
2020-08-16 13:49:57 +0500

    task III in progress

--- Home task III/src/Home task III.c
+++ Home task III/src/Home task III.c
@@ -10,8 +10,34 @@
 
 #include <math.h>
 
+
+/*
+//3.1
+
 int calcSquare(int a, int b, int c, double* x1, double* x2)	{
 
 	return 0;
 }
 
+*/
+
+//3.2
+
+int main (int a, const char** argv)	{
+	printf("Введите любое целое число: ");
+	scanf("%d", &a);
+	for ( int b = 0; b <= a; b++ )	{
+		printf ("%d", b);
+	}
+	return 0;
+}
+
+int arr ()	{
+	arr[]
+}
+
+/*
+int main (int argc, const char** argv)	{
+
+}
+*/
